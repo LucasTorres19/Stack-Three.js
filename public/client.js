e43855b53547f1c6552fdab140d0838e6a54b44c
Thiago-cpu <thifran789456323@gmail.com>
2021-05-11 19:13:41 -0300

    Creando Base

--- public/client.js
+++ public/client.js
@@ -1,8 +1,11 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
 import {OrbitControls} from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+import setup from './functions/setup.js'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const aspect = window.innerWidth / window.innerHeight;
+const d = 20;
+const camera = new THREE.OrthographicCamera( - d * aspect, d * aspect, d, - d, 1, 1000 )
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -12,32 +15,26 @@ window.addEventListener('resize', () => {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    render()
+    renderer.render( scene, camera );
 }, false);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
 
-
-scene.add( cube );
-const geometry2 = new THREE.BoxGeometry();
-const material2 = new THREE.MeshBasicMaterial( { color: 0x123456 } );
-const cube2 = new THREE.Mesh( geometry2, material2 );
-cube2.position.x = 1
-
-scene.add( cube2 );
-console.log(window.innerWidth / window.innerHeight)
-camera.position.z = 5;
+const setup = function () {
+    const base = base()
+    scene.add( base );
+    camera.position.set( 20, 20, 20 ); 
+    camera.lookAt( scene.position ); 
+}
 
 const animate = function () {
     requestAnimationFrame( animate );
+    // cube.position.x += 0.1
     
-   
-    render()
+    renderer.render( scene, camera );
     
 };
-function render(){
-    renderer.render( scene, camera );
-}
+
+
+
+setup()
 animate();
---
+++ public/functions/setup.js
@@ -0,0 +1,8 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
+export default function base(){
+    const geometry = new THREE.BoxGeometry(5,30,5);
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const base = new THREE.Mesh( geometry, material );
+    base.position.set(0,base.position.y - base.geometry.parameters.height / 2,0)
+    return base
+}
